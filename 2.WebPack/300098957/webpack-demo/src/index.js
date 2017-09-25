87972ea39f808b63c9c72005a8256f3d0fac45af
Brice Robert <300098957@collegeboreal.ca>
2017-09-25 10:59:51 -0400

    Duh

--- 2.WebPack/300098957/webpack-demo/src/index.js
+++ 2.WebPack/300098957/webpack-demo/src/index.js
@@ -1,7 +1,9 @@
+import _ from 'lodash';
+
 function component() {
   var element = document.createElement('div');
 
-  // Lodash, currently included via a script, is required for this line to work
+  // Lodash, nom imported by script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   return element;
