bd9ffa4121f45939a51c67d311b028d73c401a7f
Brice Robert <300098957@collegeboreal.ca>
2017-09-29 15:35:01 -0400

    generate component

--- 3.Angular/B300098957/src/app/app.component.ts
+++ 3.Angular/B300098957/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Brice';
+  title = 'Transfert d argent';
 }
--- 3.Angular/B300098957/src/app/app.module.ts
+++ 3.Angular/B300098957/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UtilisateurComponent } from './utilisateur/utilisateur.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UtilisateurComponent
   ],
   imports: [
     BrowserModule
