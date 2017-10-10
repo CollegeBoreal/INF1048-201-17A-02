3fa7b87c058161f8c56171070a575603729a261e
Brice Robert <300098957@collegeboreal.ca>
2017-10-10 12:53:22 -0400

    Service

---
+++ 5.DataList/B300098957/src/app/pokemon.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+import 'rxjs/Rx';
+
+@Injectable()
+export class PokemonService {
+
+  constructor(private http: Http) {
+
+  }
+
+  pokemonInit(){
+    let pokemons: Array<any> = [];
+
+    this.http.get('https://pokeapi.co/api/v2/pokemon?limit=151')
+    .map( tout => tout.json() )
+    .flatMap( tableau => tableau.results )
+    .subscribe(
+      donnee => {
+        pokemons.push(donnee)
+      },
+      erreur => {
+        console.error(erreur)
+      }
+    );
+
+    return pokemons;
+  }
+
+}
