523131c5f590e5567a729ee8919bc64e54636c73
Sowmdo0968 <sowmkb@yahoo.com>
2017-11-24 15:44:46 -0500

    suite

--- 6.Theme/B300102828/src/app/app.module.ts
+++ 6.Theme/B300102828/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {ThemeModule} from './@theme/theme.module';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -10,6 +11,7 @@ import {ThemeModule} from './@theme/theme.module';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     ThemeModule.forRoot()
   ],
   providers: [],
---
+++ 6.Theme/B300102828/src/app/pages/pages.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pages',
+  template: `
+    <p>
+      pages works!
+    </p>
+  `,
+  styles: []
+})
+export class PagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
