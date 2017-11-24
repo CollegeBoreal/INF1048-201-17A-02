cd092669e4ec2ff43f27fc7b9d88813c199725d4
Setra <setrar@valiha.com>
2017-11-24 18:25:35 -0500

    ionic

--- 6.Theme/B300098957/src/app/pages/pages-menu.ts
+++ 6.Theme/B300098957/src/app/pages/pages-menu.ts
@@ -3,12 +3,13 @@ import {NbMenuItem} from '@nebular/theme';
 export const MENU_ITEMS: NbMenuItem[] = [
   {
     title: 'Dashboard',
-    icon: 'nb-home',
+    icon: 'ion ion-speedometer',
     link: '/pages/dashboard',
     home: true,
   },
   {
     title: 'FEATURES',
+    icon: 'ion ion-ios-keypad-outline',
     group: true,
   }
 ];
