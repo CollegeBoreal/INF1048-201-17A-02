084d9296dad7b40e4239c55457273bb96534c04e
Sowmdo0968 <sowmkb@yahoo.com>
2017-11-24 15:20:21 -0500

    Suite du projet

---
+++ 6.Theme/B300102828/src/app/@theme/layout/layout-one-column/layout-one-column.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-one-column',
+  template: `
+    <nb-layout>
+      <nb-layout-header fixed>Company Name</nb-layout-header>
+
+      <nb-sidebar>Sidebar Content</nb-sidebar>
+
+      <nb-layout-column>Page Content</nb-layout-column>
+    </nb-layout>
+    `,
+  styleUrls: ['./layout-one-column.component.scss']
+})
+export class LayoutOneColumnComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- 6.Theme/B300102828/src/app/@theme/theme.module.ts
+++ 6.Theme/B300102828/src/app/@theme/theme.module.ts
@@ -1,18 +1,34 @@
 import {ModuleWithProviders, NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {NbLayoutModule, NbSidebarModule, NbThemeModule} from '@nebular/theme';
+import { LayoutOneColumnComponent } from './layout/layout-one-column/layout-one-column.component';
 
 const BASE_MODULES = [CommonModule, FormsModule, ReactiveFormsModule];
 
+const NB_MODULES = [
+  NbLayoutModule,
+  NbSidebarModule,
+];
+
+const NB_THEME_PROVIDERS = [
+  ...NbThemeModule.forRoot({ name: 'default' }).providers,
+  ...NbSidebarModule.forRoot().providers
+];
+
+const COMPONENTS = [
+  LayoutOneColumnComponent
+]
 @NgModule({
-  imports: [...BASE_MODULES],
-  exports: [...BASE_MODULES],
-  declarations: []
+  imports: [...BASE_MODULES, ...NB_MODULES],
+  exports: [...BASE_MODULES, ...NB_MODULES, ...COMPONENTS],
+  declarations: [LayoutOneColumnComponent]
 })
 export class ThemeModule {
   static forRoot(): ModuleWithProviders {
     return <ModuleWithProviders>{
       ngModule: ThemeModule,
+      providers: [...NB_THEME_PROVIDERS],
     };
   }
 }
