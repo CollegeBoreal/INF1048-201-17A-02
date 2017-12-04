5759cf521620618bc00e09313b31b197a53f5a36
Sowmdo0968 <sowmkb@yahoo.com>
2017-12-04 12:12:14 -0500

    projet suite

---
+++ 8.Layout/B300102828/src/app/@theme/layout/layout-one-column/layout-one-column.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-one-column',
+  template: `
+    <nb-layout>
+      <nb-layout-header fixed>Company Name</nb-layout-header>
+
+      <nb-sidebar>
+        <ng-content select="nb-menu"></ng-content>
+      </nb-sidebar>
+
+      <nb-layout-column>
+        <ng-content select="router-outlet"></ng-content>
+      </nb-layout-column>
+      <nb-layout-footer fixed> MS Tranfert</nb-layout-footer>
+    </nb-layout>
+  `,
+  styleUrls: ['./layout-one-column.component.scss']
+})
+export class LayoutOneColumnComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ 8.Layout/B300102828/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import {Routes, RouterModule, ExtraOptions} from '@angular/router';
+
+const routes: Routes = [
+  { path: 'pages', loadChildren: 'app/pages/pages.module#PagesModule' },
+  { path: '', redirectTo: 'pages', pathMatch: 'full' },
+  { path: '**', redirectTo: 'pages' },
+];
+
+const config: ExtraOptions = {
+  useHash: true,
+};
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, config)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
