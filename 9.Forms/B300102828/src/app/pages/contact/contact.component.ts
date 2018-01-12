92338992e4b4ddc0fc6d1083f3132eae725aca3d
Sowmdo0968 <sowmkb@yahoo.com>
2018-01-12 13:28:06 -0500

    projet final

--- 9.Forms/B300102828/src/app/pages/contact/contact.component.ts
+++ 9.Forms/B300102828/src/app/pages/contact/contact.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
-
+  powers = ['Email','Telphone','Chat',
+  ];
   constructor() { }
 
   ngOnInit() {
